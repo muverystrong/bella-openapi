64d524ee63ba8928880e0898aeb615ebde3d5fb9
jiand.yuan001 <jiand.yuan001@ke.com>
2026-03-24 13:12:16 +0800

    feat: redis streaming cut window set to 10 min

--- api/spi/src/main/java/com/ke/bella/queue/worker/Worker.java
+++ api/spi/src/main/java/com/ke/bella/queue/worker/Worker.java
@@ -139,7 +139,7 @@ public class Worker {
 
     private void sendMessage(String instanceId, TaskWrapper.Event<?> event) {
         String streamKey = eventbus.getTopic() + instanceId;
-        long minIdTimestamp = System.currentTimeMillis() - 60_000;
+        long minIdTimestamp = System.currentTimeMillis() - 60_000 * 10;
 
         try (Jedis jedis = jedisPool.getResource()) {
             jedis.xadd(streamKey,
